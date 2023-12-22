2691cba5f6b0444e683c4fc5f4be5ce1e820cbd1
phil1436 <philxsb@gmail.com>
2023-12-23 00:00:34 +0100

    updaet

--- src/app/desktop-file/desktop-file.component.spec.ts
+++ src/app/desktop-file/desktop-file.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { DesktopFileComponent } from './desktop-file.component';
 
 describe('DesktopFileComponent', () => {
-  let component: DesktopFileComponent;
-  let fixture: ComponentFixture<DesktopFileComponent>;
+	let component: DesktopFileComponent;
+	let fixture: ComponentFixture<DesktopFileComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [DesktopFileComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(DesktopFileComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [DesktopFileComponent],
+		}).compileComponents();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+		fixture = TestBed.createComponent(DesktopFileComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
+
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
--- src/app/dock/dock.component.spec.ts
+++ src/app/dock/dock.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { DockComponent } from './dock.component';
 
 describe('DockComponent', () => {
-  let component: DockComponent;
-  let fixture: ComponentFixture<DockComponent>;
+	let component: DockComponent;
+	let fixture: ComponentFixture<DockComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [DockComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(DockComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [DockComponent],
+		}).compileComponents();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+		fixture = TestBed.createComponent(DockComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
+
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
