80d6153d4e84ae4d41db3126da38654d5baddc2a
phil1436 <philxsb@gmail.com>
2023-12-24 15:42:42 +0100

    Refactor AppIconComponent unit test

--- src/app/app-icon/app-icon.component.spec.ts
+++ src/app/app-icon/app-icon.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppIconComponent } from './app-icon.component';
 
 describe('AppIconComponent', () => {
-  let component: AppIconComponent;
-  let fixture: ComponentFixture<AppIconComponent>;
+	let component: AppIconComponent;
+	let fixture: ComponentFixture<AppIconComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppIconComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(AppIconComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [AppIconComponent],
+		}).compileComponents();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+		fixture = TestBed.createComponent(AppIconComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
+
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
